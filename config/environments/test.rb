73f8bf1cfe10bd4cdd08247c06a108b799bc7600
Zach Abrahams <zachabrahams@gmail.com>
2014-09-06 07:34:52 -0500

    add basic profile page

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcryt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
