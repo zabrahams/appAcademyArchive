6a542641f39d45d93312f6ab1a8bb1338145e56a
Zach Abrahams <zachabrahams@gmail.com>
2014-09-12 21:54:22 -0500

    complete basic user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :signed_in_user, only: [:edit, :update]
+ before_action :correct_user,   only: [:edit, :update]
   
   def show
   	@user = User.find(params[:id])
@@ -20,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -38,6 +38,20 @@ class UsersController < ApplicationController
     def user_params
   	  params.require(:user).permit(:name, :email, :password, 
   		                             :password_confirmation)
+    end
+    
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in" 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
 
