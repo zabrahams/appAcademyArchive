6abb1b5ffae39d014fa23e585cb973abf890e7fb
Zach Abrahams <zachabrahams@gmail.com>
2014-09-24 21:22:59 -0400

    implement the web interface for following and unfollowing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,6 +32,17 @@ describe "StaticPages" do
         end
       end
 
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
       describe "should display and pluralize the micropost count" do
 
         describe "for multiple microposts" do
