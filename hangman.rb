b829580efee0e7ea576a62376df0f85ea5949d3a
Zach Abrahams <zachabrahams@gmail.com>
2014-11-16 00:06:26 -0500

    Stubbed out Player Interface

--- hangman.rb
+++ hangman.rb
@@ -4,8 +4,38 @@ end
 
 class HumanPlayer
 
+  def pick_secret_word
+  end
+
+  def receive_secret_length
+  end
+
+  def guess
+  end
+
+  def check_guess
+  end
+
+  def handle_guess_response
+  end
+
 end
 
 class ComputerPlayer
+
+  def pick_secret_word
+  end
+
+  def receive_secret_length
+  end
+
+  def guess
+  end
+
+  def check_guess
+  end
+
+  def handle_guess_response
+  end
 
 end
