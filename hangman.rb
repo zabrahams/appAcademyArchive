6a32493f6d2d816f37c6d2f47dc314110f225831
Zach Abrahams <zachabrahams@gmail.com>
2014-11-16 00:15:18 -0500

    computer player picks secret word

--- hangman.rb
+++ hangman.rb
@@ -22,8 +22,11 @@ class HumanPlayer
 end
 
 class ComputerPlayer
+  DICT_FILE = "dictionary.txt"
 
   def pick_secret_word
+    dictionary = load_dictionary
+    @secret_word = dictionary.sample
   end
 
   def receive_secret_length
@@ -35,7 +38,17 @@ class ComputerPlayer
   def check_guess
   end
 
+  def reveal_word
+  end
+
   def handle_guess_response
+  end
+
+  private
+  attr_writer :secret_word
+
+  def load_dictionary
+    File.readlines(DICT_FILE).map(&:chomp)
   end
 
 end
