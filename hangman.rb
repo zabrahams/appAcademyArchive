f1c70bbdf8380959315eefbc7138677c9377355c
Zach Abrahams <zachabrahams@gmail.com>
2014-11-16 01:34:07 -0500

    complete game logic and cli

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,48 @@
 class Hangman
 
+  attr_accessor :board, :master, :guesser, :current_turn
+  MAX_TURNS = 10
+
+  def initialize(master, guesser)
+    @master = master
+    @guesser = guesser
+  end
+
+  def run
+    master.pick_secret_word
+    @board = Array.new(master.secret_length)
+    @current_turn = 0
+
+    until (won? || current_turn == MAX_TURNS)
+      play_turn
+    end
+
+    if won?
+      puts "You did it! You guessed '#{render_board}'"
+    else
+     puts "You couldn't even get an easy word like '#{master.reveal_word}'. Shame on you."
+   end
+  end
+
+  def play_turn
+    puts "#{render_board}    Turn: #{current_turn + 1}"
+    guess = guesser.guess
+    update_board(guess, master.check_guess(guess))
+    self.current_turn += 1
+  end
+
+  def won?
+    board.none?(&:nil?)
+  end
+
+  def update_board(guess, indices)
+    indices.each { |i| board[i] = guess }
+  end
+
+  def render_board
+    board.map { |letter| letter.nil? ? "_" : letter }.join("")
+  end
+
 end
 
 class HumanPlayer
@@ -30,7 +73,7 @@ class HumanPlayer
   def check_guess
   end
 
-  def return_guess_indices
+  def guess_indices
   end
 
   def handle_guess_response
@@ -54,10 +97,6 @@ class ComputerPlayer
   end
 
   def check_guess(guess)
-    secret_word.include?(guess)
-  end
-
-  def return_guess_indices(gues)
     (0...secret_word.length).select { |i| secret_word[i] == guess }
   end
 
@@ -76,4 +115,11 @@ class ComputerPlayer
     File.readlines(DICT_FILE).map(&:chomp)
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  hum = HumanPlayer.new
+  comp = ComputerPlayer.new
+  game = Hangman.new(comp, hum)
+  game.run
 end
