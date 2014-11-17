ced36d4896d082940eff37858b0ee72a27145d7a
Anthony and Zach <anthony+zach@appacademy.io>
2014-11-17 15:05:01 -0500

    Continues working on play method. Adds screen clear.

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,25 +8,34 @@ class Minesweeper
   end
 
   def play
-    @board.display
-    puts "What is your move? (h for help)"
-    print ">"
-
-    begin
-      input = gets.chomp
-
-      case input[0]
-      when "f"
-      when "r"
-      when "h"
-        help
-      else
-        raise "Invalid input"
-      end
-    rescue
-      puts "Invalid input!"
+    while true
+      @board.display
+      puts "What is your move? (h for help, and q for quit)"
       print ">"
-      retry
+
+      begin
+        input = gets.chomp
+        coords = /[fr]\((\d*)\,(\d*)\)/.match(input)
+        x, y = coords[1].to_i, coords[2].to_i unless coords.nil?
+        system "clear"
+
+        case input[0]
+        when "f"
+          @board[x,y].mark
+        when "r"
+          @board[x,y].reveal
+        when "h"
+          help
+        when "q"
+          break
+        else
+          raise "Invalid input"
+        end
+      rescue
+        puts "Invalid input!"
+        print ">"
+        retry
+      end
     end
   end
 
@@ -43,6 +52,19 @@ class Minesweeper
     puts "* - Unmarked tile"
     puts "r(coordinates) - reveals a tile"
     puts "f(coordinates) - flags a tile"
+  end
+
+  def reveal_neighbors(tile)
+    tile.reveal
+    return if tile.neighbor_bomb_count > 0
+
+    tile.neighbors.each do |neighbor|
+      reveal_neighbors(neighbor) unless neighbor.marked
+    end
+  end
+
+  def lose?(x, y)
+    @board[x, y].bombed
   end
 
 end
