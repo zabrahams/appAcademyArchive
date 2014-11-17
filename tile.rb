ba2e595152a5691cc9a9061ca421d40c64046702
Anthony and Zach <anthony+zach@appacademy.io>
2014-11-17 11:17:49 -0500

    Adds initialize methods.

---
+++ tile.rb
@@ -0,0 +1,17 @@
+class Tile
+  def initialize
+    @bombed = false
+    @revealed = false
+    @marked = false
+  end
+
+  def reveal
+  end
+
+  def neighbors
+  end
+
+  def neighbor_bomb_count
+  end
+
+end
