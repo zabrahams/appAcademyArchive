5de561ca5b7bcd6c7a71d8ddafd3b4336cb2c27e
Anthony and Zach <anthony+zach@appacademy.io>
2014-11-17 11:22:34 -0500

    Adds neighbor bomb count and reveal methods.

--- tile.rb
+++ tile.rb
@@ -1,4 +1,7 @@
 class Tile
+
+  attr_reader :bombed
+
   def initialize
     @bombed = false
     @revealed = false
@@ -6,12 +9,16 @@ class Tile
   end
 
   def reveal
+    @revealed = true
   end
 
   def neighbors
   end
 
   def neighbor_bomb_count
+    bomb_count = 0
+    bomb_count += 1 if self.neighbors.bombed
+    bomb_count
   end
 
 end
