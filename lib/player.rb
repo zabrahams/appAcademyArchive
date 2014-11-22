1beb37ae42a23478b2d97b917d8595dd95efbbaa
Zach Abrahams <zachabrahams@gmail.com>
2014-11-22 16:13:40 -0500

    Completes Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,28 @@
 class InvalidInputError < StandardError
 end
 
+class BettingError < StandardError
+end
+
 require "colorize"
 
 class Player
 
-  attr_accessor :hand, :pot
+  attr_accessor :hand
+  attr_reader :pot
 
   def initialize(pot)
     @hand = nil
     @pot = pot
+    @fold = false
+  end
+
+  def fold?
+    @fold
+  end
+
+  def receive_pot(new_pot)
+    @pot += new_pot
   end
 
   def get_discard
@@ -33,34 +46,73 @@ class Player
   end
 
   def bet_response(bet)
+    begin
+      puts "The current bet is #{"#{bet}".blink}."
+      puts "would you like to (f)old, (s)ee, or (r)aise?"
+      # should add "all-in"
+      print ">".blink
+      response = gets.chomp.downcase
 
+      unless response =~ /\A[f|s|r]\z/
+        raise InvalidInputError.new "You entered an invalid character."
+      end
+
+      case response
+      when "f"
+        fold
+      when "s"
+        see(bet)
+      when "r"
+        amt = get_raise_amount
+        bet_raise(bet, amt)
+      end
+
+    rescue InvalidInputError, BettingError => e
+      puts e.message
+      puts "Please try again."
+      retry
+    end
+  end
+
+  def get_raise_amount
+    puts "Please input the amount you would like to raise."
+    begin
+      print ">".blink
+      Integer(gets.chomp)
+    rescue ArgumentError
+      puts "Please input an integer value."
+      retry
+    end
   end
 
   def validate_indices(indices)
     unless indices.uniq == indices
-      raise InvalidInputError.new "You can only a card once."
+      raise InvalidInputError.new "You can only discard a card once."
     end
 
-    if indices.count > 2
+    if indices.count > 3
       raise InvalidInputError.new "You are attempting to discard too many cards"
     end
   end
 
   def fold
     hand = nil
+    @fold = true
+    0
   end
 
   def see(bet)
     raise BettingError.new "Not enough money!" if bet > pot
-    self.pot -= bet
+    @pot -= bet
     bet
   end
 
-  def raise(bet, raise)
-    raise BettingError.new "Not enough money!" if bet + rasie > pot
-    if new_bet <= bet
-    self.pot -= bet + raise
-    bet + raise
+  def bet_raise(bet, amt)
+    if bet + amt > pot
+      raise BettingError.new "Not enough money!"
+    end
+    @pot -= bet + amt
+    bet + amt
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ require "player"
 describe Player do
   subject(:player) { Player.new(100) }
 
-  describe "initializes" do
+  describe "#initialize" do
 
     it "with an empty hand" do
       expect(player.hand).to be nil
@@ -12,6 +12,14 @@ describe Player do
 
     it "with a pot" do
       expect(player.pot).to eq(100)
+    end
+  end
+
+  describe "#receive_pot" do
+
+    it "should add the recieved amount to player's pot" do
+      player.receive_pot(100)
+      expect(player.pot).to eq(200)
     end
   end
 
