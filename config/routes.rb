08a82170574906603a8f9cd666fd506d4a43668a
Zach Abrahams <zachabrahams@gmail.com>
2014-12-08 12:34:26 -0500

    Adds band controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Music::Application.routes.draw do
   resource :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 
+  resources :bands do
+    resource :album, only: :new
+  end
+
+  resources :albums, only: [:create, :edit, :show, :update, :destroy] do
+    resource :track, only: :new
+  end
+
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy]
+
 end
