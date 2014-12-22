c8fd376e8b60695bf4ea3cc27a4ec045928f19e3
Jesus and Zach <jz@appacademy.io>
2014-12-22 10:53:46 -0500

    Adds RootView#addPokemonToList

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,16 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $li = $("<li></li>").addClass("poke-list-item");
+  $li.append(pokemon.get("name") + " " + pokemon.get("poke_type"));
+  this.$pokeList.append($li);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
+  var that = this;
+  this.pokes.fetch({
+    success: function(pokemons) {
+      that.pokes.forEach(function(pokemon){
+        that.addPokemonToList(pokemon);
+      })
+    }
+  });
 };
