dfc3780ac6d1a9f29c736dfe17f062242f22a25f
Zach and Jake <zach+jake@appacademy.io>
2014-12-30 12:04:11 -0500

    refresh button

--- app/assets/javascripts/views/feeds/feed_show.js
+++ app/assets/javascripts/views/feeds/feed_show.js
@@ -6,8 +6,16 @@ NewsReader.Views.FeedShow = Backbone.View.extend({
     this.listenTo(this.model, "sync", this.render);
   },
 
+  events: {
+    "click button.refresh": "refreshFeed"
+  },
+
   render: function () {
     this.$el.html(this.template( { feed: this.model, entries: this.model.entries() } ));
     return this;
+  },
+
+  refreshFeed: function () {
+    this.model.fetch({success: this.render.bind(this)});
   }
 })
